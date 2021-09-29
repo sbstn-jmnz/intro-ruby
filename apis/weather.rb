5094fc54bd58c5578ca94652e71a90c5710e6509
sbstn-jmnz <sbstn.jmnz@yahoo.com>
2021-09-29 10:08:21 -0300

    api first demo

---
+++ apis/weather.rb
@@ -0,0 +1,13 @@
+require 'uri'
+require 'net/http'
+
+# Uniform Resource identifier 
+url = URI("http://api.openweathermap.org/data/2.5/weather?q=Vi%C3%B1a%20del%20mar&appid=9484ee17fae08ece349011f24f3ccc6b")
+# Crear un cliente http
+http = Net::HTTP.new(url.host, url.port)
+# Generar un solicitud
+request = Net::HTTP::Get.new(url)
+# Obtener la respuesta
+response = http.request(request)
+
+puts response.read_body
